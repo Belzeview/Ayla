947c73e7963a3d2a1cfc72c1b5eb018b8716f294
Jacob Kulwicki <142302760+Belzeview@users.noreply.github.com>
2023-09-28 17:47:19 +0200

    First run

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 
-
+/*!
+ * @brief Entry point for application
+ * @param argc 
+ * @param argv 
+ * @return 0 if succeded, 1 if failed
+*/
 int main(int argc, char** argv)
 {
+	// Debug shit here
 	printf("%s", "Hello world from Ayla Engine");
 	return 0;
 
-}
+} 
